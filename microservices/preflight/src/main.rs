5ff05d45500e940fee30aa64cfab0340f6dfda3b
Roger Lew <rogerlew@gmail.com>
2023-10-04 10:42:53 -0700

    echo server

--- microservices/preflight/src/main.rs
+++ microservices/preflight/src/main.rs
@@ -1,20 +1,61 @@
+use std::collections::HashMap;
+use std::sync::Arc;
+use tokio::sync::Mutex;
 use warp::Filter;
+use warp::ws::WebSocket;
+use futures::stream::StreamExt; // Using futures crate
 
-#[tokio::main]
+async fn handle_connection(
+    mut websocket: WebSocket,
+    connections: Arc<Mutex<HashMap<String, WebSocket>>>
+) {
+    if let Some(result) = websocket.next().await {
+        match result {
+            Ok(msg) => {
+                if msg.is_text() || msg.is_binary() {
+                    let run_id = msg.to_str().unwrap_or_default().to_string();
+                    println!("run_id: {}", &run_id);
+                    connections.lock().await.insert(run_id, websocket);
+                }
+            },
+            Err(e) => {
+                eprintln!("Error reading message: {:?}", e);
+            }
+        }
+    }
+}
+
+
+#[tokio::main(flavor = "current_thread")]
 async fn main() {
-    // Define a warp filter for a WebSocket endpoint
-    let websocket_route = warp::path("socket")
-        .and(warp::ws())
-        .map(|ws: warp::ws::Ws| {
-            ws.on_upgrade(|_websocket| {
-                // Handle the WebSocket connection here...
-                futures::future::ready(())
-            })
-        });
+    let connections = Arc::new(Mutex::new(HashMap::new()));
 
-    // Start the warp server
+let websocket_route = warp::path("socket")
+    .and(warp::ws())
+    .map({
+        let connections = Arc::clone(&connections);
+        move |ws: warp::ws::Ws| {
+            ws.on_upgrade({
+                let connections = Arc::clone(&connections); // Clone connections here
+                move |websocket| {
+                    // Handle the connection within the async block instead of spawning a new task
+                    async {
+                        handle_connection(websocket, connections).await;
+                    }
+                }
+            })
+        }
+    });
+
+let local = tokio::task::LocalSet::new();
+
+local.spawn_local(async {
     warp::serve(websocket_route)
         .run(([127, 0, 0, 1], 3030))
         .await;
+});
+
+local.await;
+
 }
 
--- microservices/preflight/tests/test_client.py
+++ microservices/preflight/tests/test_client.py
@@ -6,7 +6,7 @@ ws = websocket.WebSocket()
 ws.connect("ws://localhost:3030/socket")
 
 # Send a message
-ws.send("Hello, Server!")
+ws.send("debonair-store")
 
 # Wait for a bit
 time.sleep(1)
