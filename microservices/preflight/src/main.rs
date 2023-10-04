fe922e26c53757835aab1abc485de1c154456bf0
Roger Lew <rogerlew@gmail.com>
2023-10-04 08:51:27 -0700

    microservice setup and tested

--- microservices/preflight/src/main.rs
+++ microservices/preflight/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    // Define a warp filter for a WebSocket endpoint
+    let websocket_route = warp::path("socket")
+        .and(warp::ws())
+        .map(|ws: warp::ws::Ws| {
+            ws.on_upgrade(|_websocket| {
+                // Handle the WebSocket connection here...
+                futures::future::ready(())
+            })
+        });
+
+    // Start the warp server
+    warp::serve(websocket_route)
+        .run(([127, 0, 0, 1], 3030))
+        .await;
 }
+
---
+++ microservices/preflight/tests/test_client.py
@@ -0,0 +1,17 @@
+import websocket
+import time
+
+# Create a WebSocket connection
+ws = websocket.WebSocket()
+ws.connect("ws://localhost:3030/socket")
+
+# Send a message
+ws.send("Hello, Server!")
+
+# Wait for a bit
+time.sleep(1)
+
+# Close the connection
+ws.close()
+
+print('success')
