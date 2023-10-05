33ada5aafe8b1d4585c03e0acbba2826e1409366
Roger Lew <rogerlew@gmail.com>
2023-10-05 14:54:51 -0700

    functional async preflight server

--- microservices/preflight/preflight_server.py
+++ microservices/preflight/preflight_server.py
@@ -3,9 +3,13 @@ import tornado.websocket
 import json
 import os
 import datetime
-import redis
+import aioredis
 import asyncio
+import async_timeout
 
+
+# in order to listen to notification need to set the following in /etc/redis/redis.conf
+# notification-keyspace-events Ksh
 
 class WebSocketHandler(tornado.websocket.WebSocketHandler):
     clients = set()
@@ -45,40 +49,37 @@ class WebSocketHandler(tornado.websocket.WebSocketHandler):
                 print("Closing stale connection")
                 client.close()
 
+
     @classmethod
     async def listen_to_redis(cls):
-        print('listenting to redis')
-        # Set up Redis client
-        r = redis.Redis(host='localhost', port=6379, db=0)
-        p = r.pubsub(ignore_subscribe_messages=True)
-        
-        # Subscribe to the relevant Redis channel
-        p.subscribe('your_redis_channel')
-        
-        # Asyncio event loop to listen to Redis and not block Tornado
-        loop = asyncio.get_event_loop()
+        redis = await aioredis.from_url('redis://localhost', db=0)
+        pubsub = redis.pubsub()
+        await pubsub.psubscribe('__keyspace@0__:*')
+        future = asyncio.ensure_future(on_hset(pubsub, redis, cls.clients))
+        await future
 
-        # Listen for messages
-        while True:
-            message = await loop.run_in_executor(None, p.get_message)
-            if message:
-                # Check if the message is a keyspace notification
-                if message['type'] == 'pmessage':
-                    # Extract the key name from the channel name
+
+async def on_hset(channel: aioredis.client.PubSub, redis, clients):
+    while True:
+        try:
+            async with async_timeout.timeout(1):
+                message = await channel.get_message(ignore_subscribe_messages=True)
+                if message is not None:
                     run_id = message['channel'].split(b':')[-1].decode('utf-8')
-                    # Use hgetall to get all key-value pairs from the hash
-                    key_value_pairs = r.hgetall(run_id)
-                    # Decode the bytes to string for easier use (if applicable)
-                    key_value_pairs_decoded = {
-                        k.decode('utf-8'): v.decode('utf-8') for k, v in key_value_pairs.items()}
-                    print(f"run_id: {run_id}, preflight_hashmap: {key_value_pairs_decoded}")
 
-                    self.write_message(
-                        json.dumps({"type": "preflight", "hashmap": key_value_pairs_decoded}))
+                    hashmap = await redis.hgetall(run_id)
+                    hashmap = {k.decode('utf-8'): v.decode('utf-8') for k, v in hashmap.items()}
 
-                    await asyncio.sleep(0.001)  # Prevent busy-waiting
+                    for client in clients:
+                        if client.run_id == run_id:
+                            await client.write_message(
+                                json.dumps({"type": "preflight", "hashmap": hashmap}))
 
-        print('exiting listen to redis')
+
+                await asyncio.sleep(0.01)
+        except asyncio.TimeoutError:
+            pass
+
 
 async def main():
     app = tornado.web.Application([
