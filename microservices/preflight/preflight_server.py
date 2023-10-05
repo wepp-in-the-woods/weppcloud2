7ada7064948f504e93623628a3c9e12600e18dd1
Roger Lew <rogerlew@gmail.com>
2023-10-05 13:56:10 -0700

    preflight

--- microservices/preflight/preflight_server.py
+++ microservices/preflight/preflight_server.py
@@ -1,34 +1,34 @@
 import tornado.ioloop
-import tornado.web
 import tornado.websocket
 import json
+import os
+import datetime
+import redis
+import asyncio
+
 
 class WebSocketHandler(tornado.websocket.WebSocketHandler):
     clients = set()
 
     def check_origin(self, origin):
-        # Allow WebSocket connections from anywhere.
         return True
     
-    def open(self):
-        # Add client to the clients set.
+    def open(self, run_id):
         self.clients.add(self)
+        self.run_id = run_id
+        self.last_pong = datetime.datetime.utcnow()  # Add a property to track the last pong
     
     def on_message(self, message):
-        # Handle incoming messages from the client.
         payload = json.loads(message)
         if payload.get("type") == "pong":
-            print("Received pong from client.")
-        elif payload.get("type") == "run_id":
-            self.run_id = payload.get("data")
-            # TODO: Handle client registration.
+            self.last_pong = datetime.datetime.utcnow()  # Update last pong when a pong is received
+        elif payload.get("type") == "init":
+            pass
     
     def on_close(self):
-        # Remove client from the clients set.
         self.clients.remove(self)
     
     def ping_client(self):
-        # Send a ping to the client.
         self.write_message(json.dumps({"type": "ping"}))
 
     @classmethod
@@ -36,14 +36,61 @@ class WebSocketHandler(tornado.websocket.WebSocketHandler):
         for client in cls.clients:
             client.ping_client()
 
-# Set up the Tornado application.
-app = tornado.web.Application([
-    (r"/websocket", WebSocketHandler),
-])
+    @classmethod
+    def check_clients(cls):
+        now = datetime.datetime.utcnow()
+        for client in cls.clients:
+            # If the last pong is older than 35 seconds, close the connection
+            if (now - client.last_pong).total_seconds() > 35:
+                print("Closing stale connection")
+                client.close()
+
+    @classmethod
+    async def listen_to_redis(cls):
+        print('listenting to redis')
+        # Set up Redis client
+        r = redis.Redis(host='localhost', port=6379, db=0)
+        p = r.pubsub(ignore_subscribe_messages=True)
+        
+        # Subscribe to the relevant Redis channel
+        p.subscribe('your_redis_channel')
+        
+        # Asyncio event loop to listen to Redis and not block Tornado
+        loop = asyncio.get_event_loop()
+
+        # Listen for messages
+        while True:
+            message = await loop.run_in_executor(None, p.get_message)
+            if message:
+                # Check if the message is a keyspace notification
+                if message['type'] == 'pmessage':
+                    # Extract the key name from the channel name
+                    run_id = message['channel'].split(b':')[-1].decode('utf-8')
+                    # Use hgetall to get all key-value pairs from the hash
+                    key_value_pairs = r.hgetall(run_id)
+                    # Decode the bytes to string for easier use (if applicable)
+                    key_value_pairs_decoded = {
+                        k.decode('utf-8'): v.decode('utf-8') for k, v in key_value_pairs.items()}
+                    print(f"run_id: {run_id}, preflight_hashmap: {key_value_pairs_decoded}")
+
+                    self.write_message(
+                        json.dumps({"type": "preflight", "hashmap": key_value_pairs_decoded}))
+
+                    await asyncio.sleep(0.001)  # Prevent busy-waiting
+
+        print('exiting listen to redis')
+
+async def main():
+    app = tornado.web.Application([
+        (r"/(.*)", WebSocketHandler),
+    ])
+
+    app.listen(9001)
+    tornado.ioloop.PeriodicCallback(WebSocketHandler.send_heartbeats, 30000).start()
+    tornado.ioloop.PeriodicCallback(WebSocketHandler.check_clients, 5000).start()  # Check clients every 5 seconds
+
+    await WebSocketHandler.listen_to_redis()
 
 if __name__ == "__main__":
-    app.listen(9001)
-    # Set up a periodic callback to send heartbeats every 30 seconds.
-    tornado.ioloop.PeriodicCallback(WebSocketHandler.send_heartbeats, 30000).start()
-    tornado.ioloop.IOLoop.current().start()
+    tornado.ioloop.IOLoop.current().run_sync(main)
 
