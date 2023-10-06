aca99927a809d7042396200365e9fbd208d69b72
Roger Lew <rogerlew@gmail.com>
2023-10-06 16:47:40 -0700

    functional status server

--- microservices/status/status_server.py
+++ microservices/status/status_server.py
@@ -2,16 +2,17 @@ import tornado.ioloop
 import tornado.websocket
 import aioredis
 import asyncio
+import async_timeout
 import json
+import datetime
 
 # Constants
 REDIS_URL = 'redis://localhost'
 DB = 2
 
 
-class HeartbeatMixin:
-    async def open(self):
-        self.last_pong = datetime.datetime.utcnow()
+class WebSocketHandler(tornado.websocket.WebSocketHandler):
+    clients = set()
 
     async def ping_client(self):
         try:
@@ -26,27 +27,17 @@ class HeartbeatMixin:
 
     @classmethod
     def check_clients(cls):
-        now = tornado.ioloop.IOLoop.current().time()
+        # Consider logging client checks for debugging purposes
+        now = datetime.datetime.utcnow()
         for client in cls.clients:
-            if now - client.last_pong > 35:  # seconds
+            if (now - client.last_pong).total_seconds() > 35:
                 print("Closing stale connection")
                 client.close()
-
-    async def on_message(self, message):
-        payload = json.loads(message)
-        if payload.get("type") == "pong":
-            self.last_pong = datetime.datetime.utcnow()
-    
-
-class WebSocketHandler(tornado.websocket.WebSocketHandler, HeartbeatMixin):
-    clients = set()
 
     def check_origin(self, origin):
         return True
 
     async def open(self, run_id, channel):
-        await super().open()
-
         self.run_id = run_id
         self.channel = channel
         self.last_pong = tornado.ioloop.IOLoop.current().time()  # track the last pong time
@@ -54,33 +45,46 @@ class WebSocketHandler(tornado.websocket.WebSocketHandler, HeartbeatMixin):
         self.clients.add(self)
         asyncio.ensure_future(self.listen_to_redis())
 
+        self.last_pong = datetime.datetime.utcnow()
+
+        print(run_id, channel)
+
     async def connect_to_redis(self):
         backoff = 1
         max_backoff = 64
         while True:
             try:
-                self.redis = await aioredis.create_redis(REDIS_URL, db=DB)
+                self.redis = await aioredis.from_url(REDIS_URL, db=DB)
                 break
             except Exception as e:
                 print(f"Error connecting to Redis: {e}")
                 await asyncio.sleep(backoff)
                 backoff = min(backoff * 2, max_backoff)
 
+    async def proxy_message(self, channel: aioredis.client.PubSub):
+        while True:
+            try:
+                async with async_timeout.timeout(1):
+                    message = await channel.get_message(ignore_subscribe_messages=True)
+                    if message is not None:
+                        print(f"(Reader) Message Received: {message}")
+                        data = message['data'].decode('utf-8')
+                        self.write_message(
+                            json.dumps({"type": "status", "data": data}))
+                    await asyncio.sleep(0.001)
+            except asyncio.TimeoutError:
+                pass
+
     async def listen_to_redis(self):
-        channel, = await self.redis.subscribe(self.channel)
-        try:
-            async for msg in channel.iter(encoding='utf-8'):
-                await self.write_message(json.dumps({"type": "status", "data": msg}))
-        except tornado.websocket.WebSocketClosedError:
-            pass
-        finally:
-            await self.redis.unsubscribe(self.channel)
-            self.redis.close()
-            await self.redis.wait_closed()
-            self.clients.remove(self)
+        self.pubsub = self.redis.pubsub()
+        await self.pubsub.subscribe(f"{self.run_id}:{self.channel}")
+        future = asyncio.ensure_future(self.proxy_message(self.pubsub))
+        await future
 
     async def on_message(self, message):
-        await super().on_message(message)
+        payload = json.loads(message)
+        if payload.get("type") == "pong":
+            self.last_pong = datetime.datetime.utcnow()
 
     def on_close(self):
         pass  # cleanup is handled in listen_to_redis finally block
