0ec25118d238bf8f94c68e1204d1f236e5ebb38a
Roger Lew <roger@gmail.com>
2024-04-28 18:50:33 -0700

    typo

--- weppcloud2/discord_bot/scripts/health_check.py
+++ weppcloud2/discord_bot/scripts/health_check.py
@@ -55,10 +55,10 @@ prism_data =  'lon=-123.0000&lat=45.0000&proc=location_data'
 
 services = [
     ('wepp.cloud'        , 'weppcloud' , 'https://wepp.cloud/weppcloud/health', GET, None, headers),
-    ('wepp.cloud'        , 'preflight' , 'wss://wepp.cloud/weppcloud-microservices/preflight/heath', GET, None, headers),
+#    ('wepp.cloud'        , 'preflight' , 'wss://wepp.cloud/weppcloud-microservices/preflight/heath', GET, None, headers),
     ('wepp.cloud'        , 'status'    , 'wss://wepp.cloud/weppcloud-microservices/status/health', GET, None, headers),
     ('dev.wepp.cloud'    , 'weppcloud' , 'https://dev.wepp.cloud/weppcloud/health', GET, None, headers),
-    ('dev.wepp.cloud'    , 'preflight' , 'wss://dev.wepp.cloud/weppcloud-microservices/preflight/heath', GET, None, headers),
+    ('dev.wepp.cloud'    , 'preflight' , 'wss://dev.wepp.cloud/weppcloud-microservices/preflight/health', GET, None, headers),
     ('dev.wepp.cloud'    , 'status'    , 'wss://dev.wepp.cloud/weppcloud-microservices/status/health', GET, None, headers),
     ('wepp.cloud'        , 'wmesque'   , 'https://wepp.cloud/webservices/wmesque/health', GET, None, headers),
     ('wepp.cloud'        , 'metquery'  , 'https://wepp.cloud/webservices/metquery/health', GET, None, headers),
@@ -76,8 +76,7 @@ async def make_ws_request(uri, retries=3, headers=None):
             async with websockets.connect(uri, timeout=10, extra_headers=headers) as websocket:                
                 response = await websocket.recv()
                 return True, timer.elapsed_ms()
-        except WebSocketException as e:
-            print(f"Attempt {attempt + 1} failed: {e}")
+        except:
             if attempt < retries - 1:
                 await asyncio.sleep(1)  # wait for 1 second before retrying if not the last attempt
     return False, None
@@ -107,22 +106,33 @@ async def request_router(url, retries=3, method=None, data=None, headers=None):
 
 
 if __name__ == "__main__":
+    from datetime import datetime
+    import json
 
-
-    channel = "health"
+    allAvailable = True
     message = []
     message.append("```")  # Start of code block to ensure monospace font
     message.append("Host              Service        ms")  # Column headers
     message.append("─" * 36 ) # U+2500 Header separator
 
+    doc = {
+        "DateTime": datetime.now().isoformat(),
+        "Services": [],
+        "Metadata": {
+            "checkedBy": fqdn
+        }
+    }
+
     for service in services:
         print(service)
         host, service, url, method, data, headers = service
-        status, ms = asyncio.run(request_router(url, retries=3, method=method, headers=headers, data=data))
-        emoji = ("\U0001F534", "\U0001F7E2")[status]
+        isAvailable, ms = asyncio.run(request_router(url, retries=3, method=method, headers=headers, data=data))
+        doc['Services'].append(dict(host=host, service=service, isAvailable=isAvailable, ms=ms))
+
+        emoji = ("\U0001F534", "\U0001F7E2")[isAvailable]
         
-        if not status:
-            channel = "error"
+        if not isAvailable:
+            allAvailable = False
 
         if ms is None:
             ms_str = ''
@@ -131,10 +141,21 @@ if __name__ == "__main__":
 
         message.append(f"{host:<17} {emoji} {service:<9} {ms_str:>5}")
     message.append("```")  # End of code block
-
-    # Join all parts of the message into a single string with newlines
     message = '\n'.join(message)
-
     print(message)
-    send_discord_message(message, channel)
+    send_discord_message(message, ('error', 'health')[allAvailable])
+
+    doc['allAvailable'] = allAvailable
+
+
+    import firebase_admin
+    from firebase_admin import firestore, initialize_app, credentials
+
+    cred = credentials.Certificate(_join(_thisdir, "weppcloud-stats-firebase-adminsdk-1j1n6-ca2cb090cd.json"))
+    firebase_admin.initialize_app(cred)
+
+    db = firestore.client()
+    db.collection('health').add(doc)
+
+    print(doc)
 
